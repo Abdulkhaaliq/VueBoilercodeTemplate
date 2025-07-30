0a2e4d051201af90ef30a11c37c37f69d5c4e192
Abdul-khaaliq Dollie <55828788+Abdulkhaaliq@users.noreply.github.com>
2025-07-30 16:27:57 +0200

    Proper fix for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/',
-  plugins: [vue()],
+  base: '',
+    plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
